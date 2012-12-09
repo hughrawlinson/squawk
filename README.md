7a2ef7326486dc1467082c834356ba42e11faad3
Hugh Rawlinson <hugh@rawlinson.com>
2012-12-09 14:08:44 +0000

    Readme

--- README.md
+++ README.md
@@ -1,1 +1,21 @@
-In Development. Check back tomorrow.
+# Squawk
+Squawk is a Twitter inspired web server that runs on a Raspberry PI and allows you to control your GPIOs remotely.
+ 
+It was developed by Hugh Rawlinson (@hughrawlinson) and Rich Hollis (@richhollis) at Code Club's Raspberry PI Hack (8/12/12 - 9/12/12). We won the best school focussed app prize.
+ 
+Hey, why not just use the Twitter API? We originally considered talking directly to Twitter but we considered the possibility that some schools will block access to social media sites like Twitter through their firewall. So we decided to create a stand-alone project that anyone could use without restriction.
+ 
+Squawk currently contains basic actions to turn on and off individual GPIO ports. For example, kids can squawk the following actions to turn GPIO 17 & 22 on and off:
+
+TURN 17 ON
+
+TURN 22 ON
+
+TURN 22 OFF
+
+We plan to add other actions in the future and make it easier to add other commands that you might want to control . We'd love to hear your suggestions on how we might make Squawk better for you and your class.
+ 
+Squawk runs as a single python script. Currently, the only library dependency is CherryPy which is used to create the web server. The script needs run as sudo or your PI needs to be configured to allow access to the GPIO ports for another user.
+
+# How To
+Squawk is a python webserver that uses the [Cherry Py](http://cherrypy.org) library, so prior to using Squawk you'll have to install that. Instructions are at the Cherry Py website. To run squawk, go to the directory into which you downloaded the source and run `sudo python squawk.py`. It will set up the GPIO pins for you. On line 227 of Squawk you will have to put in the internal IP address of your Pi on your network. You can then set up port forwarding on your router to allow external access. You can then have any computer on the network point their browsers at the Pi port 8080 (you may want to set up dns or hosts files to use a domain name internally). Each user will be able to see each other's tweets, and command the GPIO ports (with the commands listed above)
--- squawk.py
+++ squawk.py
@@ -5,52 +5,55 @@ from datetime import datetime
 from ossaudiodev import open
 
 class SquawkApp(object):
-	"""Main Squawk Server"""
+    """Main Squawk Server"""
 
-	def __init__(self):
-		"""Initialise Squawk Server"""
-		self.squawkList = []
-		print("initialised")
-		os.system("echo \"17\" > /sys/class/gpio/export")
-		os.system("echo \"22\" > /sys/class/gpio/export")
-		os.system("echo \"out\" > /sys/class/gpio/gpio17/direction")
-		os.system("echo \"out\" > /sys/class/gpio/gpio22/direction")
-	
-	def index(self):
-		"""Concatenate parts of web page"""
-		return(self.header()+self.actions()+self.messages()+self.footer())
-	index.exposed = True
+    def __init__(self):
+        """Initialise Squawk Server"""
+        self.squawkList = []
+        print("initialised")
+        os.system("echo \"17\" > /sys/class/gpio/export")
+        os.system("echo \"22\" > /sys/class/gpio/export")
+        os.system("echo \"out\" > /sys/class/gpio/gpio17/direction")
+        os.system("echo \"out\" > /sys/class/gpio/gpio22/direction")
+    
+    def index(self):
+        """Concatenate parts of web page"""
+        return(self.header()+self.actions()+self.messages()+self.footer())
+    index.exposed = True
 
-	# def squawksJson(self):
-	# 	# serve json of all squawks after time
-	# 	retstr = ""
-	# 	for squawk in self.squawkList:
-	# 		retstr = retstr+"{name:"+squawk.username+",squawk:"+squawk.squawk+"time:"+str(squawk.time)+"}"
-	# 	return(retstr)
-	# squawksJson.exposed = True
+    # def squawksJson(self):
+    #   # serve json of all squawks after time
+    #   retstr = ""
+    #   for squawk in self.squawkList:
+    #       retstr = retstr+"{name:"+squawk.username+",squawk:"+squawk.squawk+"time:"+str(squawk.time)+"}"
+    #   return(retstr)
+    # squawksJson.exposed = True
 
-	def handleGPIOCommands(self,message):
-		"""Check message for GPIO command"""
-		onGpio = GpioCommands.get_turn_on_gpio(message)
-		offGpio = GpioCommands.get_turn_off_gpio(message)
-		if(onGpio > 0):
-			os.system("echo \"1\" > /sys/class/gpio/gpio"+str(onGpio)+"/value")
+    def handleGPIOCommands(self,message):
+        """Check message for GPIO command"""
+        onGpio = GpioCommands.get_turn_on_gpio(message)
+        offGpio = GpioCommands.get_turn_off_gpio(message)
+        if(onGpio > 0):
+            os.system("echo \"1\" > /sys/class/gpio/gpio"+str(onGpio)+"/value")
 
-		if(offGpio > 0):
-			os.system("echo \"0\" > /sys/class/gpio/gpio"+str(offGpio)+"/value")
+        if(offGpio > 0):
+            os.system("echo \"0\" > /sys/class/gpio/gpio"+str(offGpio)+"/value")
 
-	def postSquawk(self,name=None,message=None,avatar=None):
-		"""Prepend squawk input to squawkList"""
-		self.handleGPIOCommands(message)
-		squawk = Squawk(message,name)
-		self.squawkList.insert(0,squawk)
-		raise cherrypy.HTTPRedirect("/")
-	postSquawk.exposed = True
+    def postSquawk(self,name=None,message=None,avatar=None):
+        """Prepend squawk input to squawkList"""
+        self.handleGPIOCommands(message)
+        squawk = Squawk(message,name)
+        self.squawkList.insert(0,squawk)
+        raise cherrypy.HTTPRedirect("/")
+    postSquawk.exposed = True
 
-	def header(self):
-		"""Return head of HTML"""
-		return('''<html>
+    def header(self):
+        """Return head of HTML"""
+        # span{font-family:"Hevletica Neue",Helvetica,Arial,sans-serif}
+        return('''<html>
 <head>
+
+<link href='http://fonts.googleapis.com/css?family=Geo' rel='stylesheet' type='text/css'>
 
 <style>
 body {
@@ -62,12 +65,13 @@ body {
 #container {
   margin: 0 auto;
   width: 920px;
-  background-color: white; opacity: 0.9;
+  opacity: 0.9;
   }
   #container:after { clear: both;}
 
-  #header { background-image: url('https://raw.github.com/hughrawlinson/squawk/master/twinkle_twinkle.png'); color: #888; text-align: left; height: 30px;}
+  #header { background-image: url('https://raw.github.com/hughrawlinson/squawk/master/twinkle_twinkle.png'); color: #888; text-align: left; height: 40px; font-family: 'Geo', sans-serif; font-size: 36px; letter-spacing: -4px; text-align: center;}
 
+body{font-family:"Hevletica Neue",Helvetica,Arial,sans-serif;font-size:12pt;font-weight:300;}
 #actions { width: 280px; float:left; background-color: #666; padding: 10px; overflow: hidden;}
 #messages-container { background-color: #666; padding: 10px; overflow: hidden;}
 #send { margin-top: 10px; padding: 4px; border-radius: 6px; background-color: silver; }
@@ -78,11 +82,15 @@ body {
 .io { height: 10px; width: 10px; padding: 2px; content: '.'; display: inline-block;}
 .io.on { background: #0a0; }
 .io.off { background: #a00; }
+.squawk { text-align: left; margin-bottom:0px;padding:8px;}
+.squawk .submit {float: right;}
+.submit{padding:0;margin:0;}
+input{margin-bottom:8px;}
 
-.message { text-align: left; padding: 8px; border-radius: 2px; }
-.message .avatar { float:left; }
-.message .body { display: inline-block; vertical-align: top; margin-top: 4px;}
-.message .body .from { font-weight: bold;}
+.message { text-align: left; padding: 8px; border-radius: 2px; margin-bottom: 2px;height:50px;}
+.message IMG { float:left; height:48px; width:48px; border: 1px solid silver; border-radius: 4px; }
+.message .body { display: inline-block; vertical-align: top; margin-top: 4px; margin-left: 4px;}
+.message .body .from { font-weight: bold; margin-left: 4px;}
 .message .time { float: right;}
 
 </style>
@@ -97,108 +105,105 @@ body {
     Squawk
   </div>''')
 
-	def actions(self):
-		"""return html send div"""
-		# return action
-		return('''<div id="actions">
+    def actions(self):
+        """return html send div"""
+        # return action
+        return('''<div id="actions">
 
   <div id="send">
-    <form method="post" action="postSquawk">
+    <form method="post" action="postSquawk" class="squawk">
     Name:
       <input type="text" name="name" />
       Message:
       <input type="text" name="message" />
-      Avatar:
-      <select name="avatar">
-      <option>Default</option>
-      </select>
-      <input type="submit" />
+      <input type="submit" class="submit"/>
+      <br style="clear:both;">
     </form>
   </div>
 
 </div>''')
 
-	def messages(self):
-		"""Return message container with message details"""
-		# return messages in html
-		return('''<div id="messages-container">
+    def messages(self):
+        """Return message container with message details"""
+        # return messages in html
+        return('''<div id="messages-container">
   <div id="messages">
-	%s
+    %s
   </div>
 </div>''' % self.messagedetail())
 
-	def footer(self):
-		"""return foot of html"""
-		return('''</div>
+    def footer(self):
+        """return foot of html"""
+        return('''</div>
 </body>
 </html>''')
 
-	def messagedetail(self):
-		"""Return each message in html"""
-		output = ""
-		for squawk in self.squawkList:
-			output += '''<div class="message">
-      <img src="http://placehold.it/48x48" />
+    def messagedetail(self):
+        """Return each message in html"""
+        output = ""
+        for squawk in self.squawkList:
+            output += '''<div class="message">
+      <img src="http://www.raspberrypi.org/wp-content/uploads/2012/03/Raspi_Colour_R.png" />
       <div class="body">
         <span class="from">%s</span><br/><span class="body">%s</span>
       </div>
       <div class="time">%s</div>
     </div>''' % (squawk.username,squawk.squawk,self.prettydate(squawk.time))
-		return output
+        return output
 
-	def prettydate(self,d):
-		"""Twitter style times"""
-	    diff = datetime.utcnow() - d
-	    s = diff.seconds
-	    if diff.days > 7 or diff.days < 0:
-	        return d.strftime('%d %b %y')
-	    elif diff.days == 1:
-	        return '1 day ago'
-	    elif diff.days > 1:
-	        return '{} days ago'.format(diff.days)
-	    elif s <= 1:
-	        return 'just now'
-	    elif s < 60:
-	        return '{} seconds ago'.format(s)
-	    elif s < 120:
-	        return '1 minute ago'
-	    elif s < 3600:
-	        return '{} minutes ago'.format(s/60)
-	    elif s < 7200:
-	        return '1 hour ago'
-	    else:
-	        return '{} hours ago'.format(s/3600)
+    def prettydate(self,d):
+        """Twitter style times"""
+        diff = datetime.utcnow() - d
+        s = diff.seconds
+        if diff.days > 7 or diff.days < 0:
+            return d.strftime('%d %b %y')
+        elif diff.days == 1:
+            return '1 day ago'
+        elif diff.days > 1:
+            return '{} days ago'.format(diff.days)
+        elif s <= 1:
+            return 'just now'
+        elif s < 60:
+            return '{} seconds ago'.format(s)
+        elif s < 120:
+            return '1 minute ago'
+        elif s < 3600:
+            return '{} minutes ago'.format(s/60)
+        elif s < 7200:
+            return '1 hour ago'
+        else:
+            return '{} hours ago'.format(s/3600)
 
 
 class Squawk(object):
-	"""Individual Squawk object"""
+    """Individual Squawk object"""
 
-	def __init__(self, squawkText, squawkName):
-		self.squawk = squawkText
-		self.username = squawkName
-		self.time = datetime.utcnow()
+    def __init__(self, squawkText, squawkName):
+        self.squawk = squawkText
+        self.username = squawkName
+        self.time = datetime.utcnow()
 
 class GpioCommands(object):
-	"""Class for Controlling GPIO"""
-	def __init__(self, arg):
-		super(GpioCommands, self).__init__()
-		self.arg = arg
+    """Class for Controlling GPIO"""
+    def __init__(self, arg):
+        super(GpioCommands, self).__init__()
+        self.arg = arg
 
-	@staticmethod
-	def get_turn_on_gpio(str):
-		"""Regex expression for turning on LED"""
-		m = re.search('^TURN\s(\d{1,2})\sON$', str, re.IGNORECASE)
-		if m is not None:
-			return int(m.group(1))
-		return 0
+    @staticmethod
+    def get_turn_on_gpio(str):
+        """Regex expression for turning on LED"""
+        m = re.search('^TURN\s(\d{1,2})\sON$', str, re.IGNORECASE)
+        if m is not None:
+            return int(m.group(1))
+        return 0
 
-	@staticmethod
-	def get_turn_off_gpio(str):
-		"""Regex expression for turning off LED"""
-		m = re.search('^TURN\s(\d{1,2})\sOFF$', str, re.IGNORECASE)
-		if m is not None:
-			return int(m.group(1))
-		return 0
+    @staticmethod
+    def get_turn_off_gpio(str):
+        """Regex expression for turning off LED"""
+        m = re.search('^TURN\s(\d{1,2})\sOFF$', str, re.IGNORECASE)
+        if m is not None:
+            return int(m.group(1))
+        return 0
 
 cherrypy.server.socket_host = '192.168.1.100'
 #cherrypy.tree.mount(SquawkApp(),"/")
